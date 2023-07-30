e55b9c3e70f44c0d7bd57c1d0f795ef8b4247094
gift kimani <gift.kimani@students.moringaschool.com>
2023-07-30 18:02:14 +0300

    comlete2

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Task({text, category, onTaskDelete}) {
+function Task({ text, category, onTaskDelete }) {
   return (
     <div className="task">
-      <div className="label">C{category}</div>
+      <div className="label">{category}</div>
       <div className="text">{text}</div>
       <button className="delete" onClick={onTaskDelete}>
         X
-        </button>
+      </button>
     </div>
   );
 }
